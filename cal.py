00196d93d65433ab06e25c3332896cd2e5c2b6ac
Himanshu Singh <himanshu.singh141007@gmail.com>
2018-10-12 23:23:21 +0530

    added add funtion

--- cal.py
+++ cal.py
@@ -6,8 +6,11 @@ def circle(rad):
 
 def square(side):
     return pow(side, 2)
+def add(x,y):
+    return x + y
 
 if __name__ == "__main__":
     print(circle(2))
     print(square(4))
+    print(add(2,3))
 
