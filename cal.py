5d769e584a5971a0a49abc9bfcd1ad588c43aed0
Himanshu Singh <himanshu.singh141007@gmail.com>
2018-10-12 23:42:24 +0530

    Subtract Function

--- cal.py
+++ cal.py
@@ -12,9 +12,13 @@ def add(x,y):
 def divide(x,y):
     return x/y
 
+def subtract(x,y):
+    return x - y
+
 if __name__ == "__main__":
     print(circle(2))
     print(square(4))
     print(add(2,3))
     print(divide(4,2))
+    print(subtract(4,3))
 
